2994a286a4239cd4f28fe8de58862ce7324f8071
Ephemera <helenawang@users.noreply.github.com>
2017-11-27 10:43:53 +0800

    Update Insertion.java

--- sort/Insertion.java
+++ sort/Insertion.java
@@ -1,14 +1,18 @@
 package sort;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Insertion extends Sort{
 	public static void sort(Comparable[] a) {
 		int N = a.length;
 		for(int i=1; i<N; i++) {
 			//insert a[i] among a[0] to a[i-1]
+			SortVisualize.paintBefore(a);
 			for(int j=i; j>0 && less(a[j], a[j-1]); j--) {
 				//a[i]的实际位置在a[0]到a[i-1]中单调向0方向移动，一旦触及比它小的，即就位。
 				exchange(a, j-1, j);
 			}
+			SortVisualize.paintAfter(a);
 		}
 	}
 	/**
@@ -28,9 +32,13 @@ public class Insertion extends Sort{
 		}
 	}
 	public static void main(String[] args) {
-		Integer[] a = {5, 4, 2, 3, 1};
-//		assert isSorted(a);
-		sort2(a);
+		int N = 30;
+		Double[] a = new Double[N];
+		for(int i=0; i<N; i++) {
+			a[i] = StdRandom.uniform();//[0, 1) uniformly
+		}
+		sort(a);
+		assert isSorted(a);
 		print(a);
 	}
 }
